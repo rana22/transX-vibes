dfa7aed581e01723dbc01f27b6ffbe47c6d308fa
arana22 <ambar.rana123@gmail.com>
2019-04-04 17:37:27 -0400

    user / role

---
+++ web/src/app/core/models/entityBase.ts
@@ -0,0 +1,13 @@
+import { Entity, EntityAspect, EntityType, MetadataStore } from 'breeze-client';
+
+export class EntityBase implements Entity {
+    entityAspect: EntityAspect;
+    entityType: EntityType;
+  
+    get $type(): string {
+      if (!this.entityAspect) return '';
+      return this.entityAspect.getKey().entityType.shortName;
+    }
+  
+  }
+  
